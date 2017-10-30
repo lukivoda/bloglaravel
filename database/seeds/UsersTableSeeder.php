bbd5d1eedecf57aecbda5be51eb94067569e7d08
Stevan Ristov <stevanris@gmail.com>
2017-10-30 22:06:17 +0100

    default admin added to UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,14 +11,32 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-       App\User::create([
+       $user = App\User::create([
            
            'name' => "Stevan Ristov",
            
            'email' => 'steve@mail.com',
            
-           'password' => bcrypt('123456')
+           'password' => bcrypt('123456'),
+
+           'admin'    =>  1
            
        ]);
+        
+        
+        App\Profile::create([
+            
+            'user_id' => $user->id,
+
+            'avatar'  => "uploads/avatars/1.jpg",
+
+            'about'   =>  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+            'facebook' => 'facebook.com',
+            
+            'youtube'  =>  'youtube.com'
+            
+        ]);
+        
     }
 }
